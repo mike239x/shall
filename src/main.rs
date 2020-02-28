9b25d976e8d5d0189d80c7961a45dca991522b3f
Mike Lezhnin <mikhail.lezhnin@esrlabs.com>
2020-02-28 12:03:06 +0100

    Write a simple typewriter
    No backspace key, nothing, only keys + enter.
    Also, 'q' is used to exit right now, so no letter 'q' as well :)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,45 @@
-use termion::{color, style};
-//use std::io;
+// use termion::{color, style};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use std::io::{Write, stdout, stdin};
 
 fn main() {
-    println!("{}Red", color::Fg(color::Red));
-    println!("{}Blue", color::Fg(color::Blue));
-    println!("{}Blue'n'Bold{}", style::Bold, style::Reset);
-    println!("{}Just plain italic{}", style::Italic, style::Reset);
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+     write!(stdout,
+           "{}{}{}",
+           termion::clear::All,
+           termion::cursor::Goto(1, 1),
+           termion::cursor::Hide)
+            .unwrap();
+
+    stdout.flush().unwrap();
+
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Char('q') => break,
+            Key::Char('\n') => print!("\n\r"),
+            Key::Char(c) => print!("{}", c),
+            Key::Alt(c) => print!("^{}", c),
+            Key::Ctrl(c) => print!("*{}", c),
+            Key::Esc => print!("ESC"),
+            Key::Left => print!("←"),
+            Key::Right => print!("→"),
+            Key::Up => print!("↑"),
+            Key::Down => print!("↓"),
+            Key::Backspace => print!("×"),
+            _ => {}
+        }
+        stdout.flush().unwrap();
+    }
+
+    write!(
+        stdout,
+        "{}{}{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),                                   
+        termion::cursor::Show
+    ).unwrap();
 }
