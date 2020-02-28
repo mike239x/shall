abc470c4b4c0a79e11c91f634a521f983fa4960d
Mike Lezhnin <mikhail.lezhnin@esrlabs.com>
2020-02-28 17:51:26 +0100

    Add "line numbers"
    Got the width/height of the terminal;
    Also now using an "AlternateScreen" to prevent any scrolling aka vim;
    The line numbers are not real, one can write on top of those -
    windows for seperate buffers (line numbers, text, powerline) are to
    come.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,46 @@
 // use termion::{color, style};
 use termion::event::Key;
-use termion::input::TermRead;
+use termion::input::{TermRead, MouseTerminal};
 use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
 use std::io::{Write, stdout, stdin};
+
+/// a window on the terminal screen
+struct Window {
+    x : u16,
+    y : u16,
+    w : u16,
+    h : u16,
+}
 
 fn main() {
     let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode().unwrap();
+    // let mut stdout = MouseTerminal::from(stdout().into_raw_mode().unwrap());
+    let mut stdout = AlternateScreen::from(stdout().into_raw_mode().unwrap());
+    write!(stdout, "{}", termion::cursor::Show).unwrap();
 
-     write!(stdout,
-           "{}{}{}",
-           termion::clear::All,
-           termion::cursor::Goto(1, 1),
-           termion::cursor::Show)
-            .unwrap();
+    let terminal_size = termion::terminal_size().unwrap();
+
+    macro_rules! clear_screen {
+        () => {
+            print!("{}", termion::clear::All);
+        };
+    }
+    macro_rules! go_to {
+        ($x:expr, $y:expr) => {
+            print!("{}", termion::cursor::Goto($x, $y));
+        };
+    }
+
+    clear_screen!();
+    for k in 1..=terminal_size.1 {
+        go_to!(1, k);
+        print!("{}", k);
+    }
+
+    print!(" {:?}", terminal_size);
+
+    go_to!(1, 1);
 
     stdout.flush().unwrap();
 
@@ -28,11 +55,6 @@ fn main() {
         stdout.flush().unwrap();
     }
 
-    write!(
-        stdout,
-        "{}{}{}",
-        termion::clear::All,
-        termion::cursor::Goto(1, 1),                                   
-        termion::cursor::Show
-    ).unwrap();
+    clear_screen!();
+    go_to!(1, 1);
 }
